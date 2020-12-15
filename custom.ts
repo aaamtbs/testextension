57a1e5d2fafaaf21a8b679b867316c25889fbf3a
aaamtbs <76047300+aaamtbs@users.noreply.github.com>
2020-12-16 07:17:43 +0800

    Initial files for MakeCode project

---
+++ custom.ts
@@ -0,0 +1,39 @@
+
+/**
+ * Use this file to define custom functions and blocks.
+ * Read more at https://makecode.microbit.org/blocks/custom
+ */
+
+enum MyEnum {
+    //% block="one"
+    One,
+    //% block="two"
+    Two
+}
+
+/**
+ * Custom1 blocks
+ */
+//% weight=100 color=#FF0000 icon="❄"
+namespace custom1 {
+    /**
+     * TODO: describe your function here
+     * @param n describe parameter here, eg: 5
+     * @param s describe parameter here, eg: "Hello"
+     * @param e describe parameter here
+     * @param e2 describe parameter here
+     */
+    //% block
+    export function foo(n: number, s: string, e: MyEnum, e2: MyEnum): void {
+        // Add code here
+    }
+
+    /**
+     * TODO: describe your function here
+     * @param value describe value here, eg: 5
+     */
+    //% block
+    export function fib(value: number): number {
+        return value <= 1 ? value : fib(value -1) + fib(value - 2);
+    }
+}
